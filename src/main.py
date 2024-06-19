9e6082f7b1fb0f3f1297e7e802e9a7ccf3111ade
yuji_matsunami <yuji.sohuto@gmail.com>
2024-06-19 21:24:33 +0900

    update workflow

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 import eventlet
+import time
 
+# https://qiita.com/ttsubo/items/9afbbfe07be928c3d47b
+
+def _sample_processing():
+    print("aaa")
+    for i in range(5):
+        print(i)
+        time.sleep(1)
+    print("finish")
+
+def main():
+    opthread = eventlet.spawn(_sample_processing)
+    opthread.wait()
 if __name__=="__main__":
     print("test!!")
     
